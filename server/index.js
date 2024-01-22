13a331f9e07859b2ee59eb17482c8dbc3ec27e41
ValentineOO <varlomxg@gmail.com>
2024-01-22 16:29:57 +0100

    created residency

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv, { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { userRoute } from "./routes/userRoute.js";
+import { residencyRoute } from "./routes/residencyRoute.js";
 dotenv.config();
 
 const app = express();
@@ -17,3 +18,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 app.use("/api/user", userRoute);
+app.use("/api/residency", residencyRoute);
