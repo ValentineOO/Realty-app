d68cf62e1790daac3728297e391a7ce0c5dbfac8
ValentineOO <varlomxg@gmail.com>
2024-01-23 14:59:27 +0100

    created allresd

--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,7 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { createResidency } from "../controllers/residencyController.js";
+import {
+  createResidency,
+  getAllResidencies,
+} from "../controllers/residencyController.js";
 
 router.post("/create", createResidency);
+router.get("/:id", getResidency);
+router.get("/allresd", getAllResidencies);
 
 export { router as residencyRoute };
