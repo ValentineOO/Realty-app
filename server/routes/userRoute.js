3d7a4d76b766b54284d18d6038d1769f54606739
ValentineOO <varlomxg@gmail.com>
2024-01-24 11:31:38 +0100

    added new features

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import {
+  bookVisit,
+  cancelBooking,
+  createUser,
+  getAllBookings,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", createUser);
+router.post("/bookVisit/:id", bookVisit);
+router.post("/allBookings", getAllBookings);
+router.post("/removeBooking/:id", cancelBooking);
 
 export { router as userRoute };
