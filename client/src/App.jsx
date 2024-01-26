b74ef3e69b90ce2a96df2f6dbba98040a510fe0d
ValentineOO <varlomxg@gmail.com>
2024-01-26 11:16:10 +0100

    added layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
+import { Suspense } from "react";
 import Website from "./pages/Website";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout"
 
 function App() {
   return (
-    <div>
-      <Website />
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Website />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Website.jsx
+++ client/src/pages/Website.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
-import Header from "../components/Header/Header";
 import Companies from "../components/Companies/Companies";
 import Residencies from "../components/Residencies/Residencies";
 import Value from "../components/Value/Value";
 import Contact from "../components/Contact/Contact";
 import GetStarted from "../components/GetStarted/GetStarted";
-import Footer from "../components/Footer/Footer";
 
 const Website = () => {
   return (
     <div className="App">
       <div>
         <div className="white-gradient" />
-        <Header />
         <Hero />
       </div>
       <Companies />
@@ -21,7 +18,6 @@ const Website = () => {
       <Value />
       <Contact />
       <GetStarted />
-      <Footer />
     </div>
   );
 };
