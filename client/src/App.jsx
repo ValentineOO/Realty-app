d008baba1cc3e56c17e0a6a082909000aaa716dc
ValentineOO <varlomxg@gmail.com>
2024-01-27 16:35:53 +0100

    setting up new features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,24 @@ import Website from "./pages/Website";
 import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Properties from "./pages/Properties/Properties";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Website />} />
-            <Route path="/properties" element={<Properties />} />
+  const queryClient = new QueryClient();
 
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
