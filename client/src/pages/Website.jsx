9f02f6861dd52f29ca712155e25944af2bb8108a
ValentineOO <varlomxg@gmail.com>
2024-01-27 14:13:40 +0100

    added more updates

--- client/src/pages/Website.jsx
+++ client/src/pages/Website.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Hero from "../components/Hero/Hero";
 import Companies from "../components/Companies/Companies";
-import Residencies from "../components/Residencies/Residencies";
-import Value from "../components/Value/Value";
 import Contact from "../components/Contact/Contact";
 import GetStarted from "../components/GetStarted/GetStarted";
+import Hero from "../components/Hero/Hero";
+import Residencies from "../components/Residencies/Residencies";
+import Value from "../components/Value/Value";
 
 const Website = () => {
   return (
