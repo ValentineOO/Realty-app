9d4107b69fbcb8ebea7b95499cd826bfb0039e9b
ValentineOO <varlomxg@gmail.com>
2024-02-12 13:33:52 +0100

    added image feature

--- client/src/components/AddPropertyModal/AddPropertyModal.jsx
+++ client/src/components/AddPropertyModal/AddPropertyModal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Container, Modal, Stepper } from "@mantine/core";
 import AddLocation from "../AddLocation/AddLocation";
 import { useAuth0 } from "@auth0/auth0-react";
+import UploadImage from "../UploadImage/UploadImage";
 
 const AddPropertyModal = ({ opened, setOpened }) => {
   const [active, setActive] = useState(0);
@@ -52,7 +53,12 @@ const AddPropertyModal = ({ opened, setOpened }) => {
             />
           </Stepper.Step>
           <Stepper.Step label="Second step" description="Verify email">
-            Step 2 content: Verify email
+            <UploadImage
+              prevStep={prevStep}
+              nextStep={nextStep}
+              propertyDetails={propertyDetails}
+              setPropertyDetails={setPropertyDetails}
+            />
           </Stepper.Step>
           <Stepper.Step label="Final step" description="Get full access">
             Step 3 content: Get full access
---
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import { AiOutlineCloudUpload } from "react-icons/ai";
+
+const UploadImage = ({
+  propertyDetails,
+  setPropertyDetails,
+  nextStep,
+  prevStep,
+}) => {
+  const [imageURL, setImageURL] = useState(propertyDetails.image);
+
+  return (
+    <div className="flexColCenter uploadWrapper">
+      {!imageURL ? (
+        <div className="flexColCenter uploadZone">
+          <AiOutlineCloudUpload size={50} color="grey" />
+        </div>
+      ) : (
+        <div className="uploadedImage">
+          <img src={imageURL} alt="" />
+          <span>Upload Image</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadImage;
