b59e579ce2f40099a020c889b5a99b70e04f49c8
ValentineOO <varlomxg@gmail.com>
2024-02-12 17:01:50 +0100

    integrated cloudinary api

--- client/src/components/UploadImage/UploadImage.jsx
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { AiOutlineCloudUpload } from "react-icons/ai";
-import "./UploadImage.css"
+import "./UploadImage.css";
 
 const UploadImage = ({
   propertyDetails,
@@ -10,11 +10,32 @@ const UploadImage = ({
   prevStep,
 }) => {
   const [imageURL, setImageURL] = useState(propertyDetails.image);
+  const cloudinaryRef = useRef();
+  const widgetRef = useRef();
+
+  useEffect(() => {
+    cloudinaryRef.current = window.cloudinary;
+    widgetRef.current = cloudinaryRef.current.createUploadWidget(
+      {
+        cloudName: "dchwohzvr",
+        uploadPreset: "crzreztm",
+        maxFiles: 1,
+      },
+      (err, result) => {
+        if (result.event === "success") {
+          setImageURL(result.info.secure_url);
+        }
+      }
+    );
+  });
 
   return (
     <div className="flexColCenter uploadWrapper">
       {!imageURL ? (
-        <div className="flexColCenter uploadZone">
+        <div
+          className="flexColCenter uploadZone"
+          onClick={() => widgetRef.current?.open()}
+        >
           <AiOutlineCloudUpload size={50} color="grey" />
         </div>
       ) : (
